48e5ccfb6d9e97616ebb2a68c1320941a44d84ee
OlhaLevchenko <165070301+OlhaLevchenko@users.noreply.github.com>
2024-05-10 01:52:27 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,64 @@
+document.getElementById("b1").addEventListener("click", f1)
+
+function f1(){
+
+  let number1 = document.getElementById("a")
+  let number2 = document.getElementById("b")
+  let result = document.getElementById("x")
+  
+  let a = parseInt(number1.value)
+  let b = parseInt(number2.value)
+  let x = parseInt(result.value)
+  let summa = a + b
+  let razn = a - b
+  let pr = a * b
+  let message = ""
+  let backgroundColor = "lightgreen"
+  let selectedValue = document.getElementById("action").value;
+  
+if(selectedValue == "summa"){
+  
+   if (x == summa){
+    message = "OK";
+  }
+  else {
+    backgroundColor = "red"
+    message = "wrong"
+  }
+}
+else if (selectedValue == "razn") {
+  if (x == razn){
+    message = "OK";
+  }
+  else {
+    backgroundColor = "red"
+    message = "wrong"
+  }
+  }
+  else if (selectedValue == "pr") {
+  if (x == razn){
+    message = "OK";
+  }
+  else {
+    backgroundColor = "red"
+    message = "wrong"
+  }
+}
+
+ document.getElementById("result").style.backgroundColor = backgroundColor
+  document.getElementById("result").innerHTML = message
+  
+  number1.value = ""
+  number2.value = ""
+  result.value = ""
+  message = ""  
+
+}
+
+
+
+
+
+
+
+
