b8cc66b1a1480f71b8084f31fea94e8b4d6bde29
OlhaLevchenko <165070301+OlhaLevchenko@users.noreply.github.com>
2024-05-14 23:19:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -57,43 +57,7 @@ let backgroundColor = "lightgreen"
   resultSel.value = ""
  
 }
-  switch (action.value) {
-    case '+':
-      summa = a + b;
-      break;
-    case '-':
-      summa = a - b;
-      break;
-    case '*':
-      summa = a * b;
-      break;
-  }
-
-  let message = ""
-  let backgroundColor = "lightgreen"
-  let selectedValue = document.getElementById("action").value;
-  
-  
- if (x == summa){
-    message = "OK";
-  }
-  else {
-    backgroundColor = "red"
-    message = "wrong"
-  }
-
- document.getElementById("result").style.backgroundColor = backgroundColor
-  document.getElementById("result").innerHTML = message
-  
-  firstNumberSel.value = ""
-  secondNumberSel.value = ""
-  operationSel.value = "none"
-  selectedValue = ""  
-  //message = ""  
-
-}
-
-
+ 
 
 
 
