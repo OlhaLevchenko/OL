c590f6fda434fdd761877bed5486cf43b5a50a03
OlhaLevchenko <165070301+OlhaLevchenko@users.noreply.github.com>
2024-05-14 17:20:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById("check").addEventListener("click", check)
 
 let firstNumberSel = document.getElementById("fn")
 let secondNumberSel = document.getElementById("b")
-let operationSel = document.getElementById("op")
+let operationSel = document.getElementById("action")
 let resultSel = document.getElementById("sum")
 
 
@@ -21,11 +21,11 @@ function check(){
   
   let a = parseInt(firstNumberSel.value)
   let b = parseInt(secondNumberSel.value)
-  let s = parseInt(resultSel.value)
+  let x = parseInt(selectedValue.value)
 
   let summa = null
   
-  switch (op.value) {
+  switch (action.value) {
     case '+':
       summa = a + b;
       break;
@@ -39,56 +39,33 @@ function check(){
 
 
 function f1(){
-
-  let number1 = document.getElementById("a")
-  let number2 = document.getElementById("b")
-  let result = document.getElementById("x")
   
-  let a = parseInt(number1.value)
-  let b = parseInt(number2.value)
-  let x = parseInt(result.value)
+  //let a = parseInt(number1.value)
+ // let b = parseInt(number2.value)
+ // let x = parseInt(result.value)
   let summa = a + b
-  let razn = a - b
-  let pr = a * b
+  //let razn = a - b
+  //let pr = a * b
   let message = ""
   let backgroundColor = "lightgreen"
   let selectedValue = document.getElementById("action").value;
   
-if(selectedValue == "summa"){
   
-   if (x == summa){
+ if (x == summa){
     message = "OK";
   }
   else {
     backgroundColor = "red"
     message = "wrong"
   }
-}
-else if (selectedValue == "razn") {
-  if (x == razn){
-    message = "OK";
-  }
-  else {
-    backgroundColor = "red"
-    message = "wrong"
-  }
-  }
-  else if (selectedValue == "pr") {
-  if (x == pr){
-    message = "OK";
-  }
-  else {
-    backgroundColor = "red"
-    message = "wrong"
-  }
-}
 
  document.getElementById("result").style.backgroundColor = backgroundColor
   document.getElementById("result").innerHTML = message
   
-  number1.value = ""
-  number2.value = ""
-  result.value = ""
+  firstNumberSel.value = ""
+  secondNumberSel.value = ""
+  operationSel.value = "none"
+  selectedValue = ""  
   message = ""  
 
 }
