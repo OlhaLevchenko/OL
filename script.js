548e77b402622c1c5a8183777c01bb2ba88f3f97
OlhaLevchenko <165070301+OlhaLevchenko@users.noreply.github.com>
2024-05-14 23:13:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,62 @@
-//document.getElementById("b1").addEventListener("click", f1)
 document.getElementById("x").addEventListener("input", checkAllInputs)
-document.getElementById("b1").addEventListener("click", check)
+document.getElementById("butt").addEventListener("click", vibor)
 
 let firstNumberSel = document.getElementById("fn")
 let secondNumberSel = document.getElementById("b")
 let operationSel = document.getElementById("action")
 let resultSel = document.getElementById("x")
-let selectedValue = document.getElementById("action").value;
+//let selectedValue = document.getElementById("action").value;
 
 
 function checkAllInputs(){
   let firstNumber = parseInt(firstNumberSel.value)
   let secondNumber = parseInt(secondNumberSel.value)
   if (Number.isInteger(firstNumber) && Number.isInteger(secondNumber) && operationSel.value != 'none'){
-  document.getElementById("b1").removeAttribute("disabled");}
+  document.getElementById("butt").removeAttribute("disabled");}
  }
 
-function check(){
+function vibor(){
   
   let a = parseInt(firstNumberSel.value)
   let b = parseInt(secondNumberSel.value)
-  let x = parseInt(selectedValue.value)
-
-  let summa = null
+  let r = parseInt(resultSel.value)
   
+  
+  let rez = null
+  
+  switch (action.value) {
+    case '+':
+      rez = a + b;
+      break;
+    case '-':
+      rez = a - b;
+      break;
+    case '*':
+      rez = a * b;
+      break;
+  }
+
+let message = ""
+let backgroundColor = "lightgreen"
+
+ if (x == rez){
+    message = "OK";
+  }
+  else {
+    backgroundColor = "red"
+    message = "wrong"
+  }
+
+
+ document.getElementById("result").style.backgroundColor = backgroundColor
+ document.getElementById("result").innerHTML = message
+  
+  firstNumberSel.value = ""
+  secondNumberSel.value = ""
+  operationSel.value = "none"
+  resultSel.value = ""
+ 
+}
   switch (action.value) {
     case '+':
       summa = a + b;
@@ -36,15 +69,6 @@ function check(){
       break;
   }
 
-
-//function f1(){
-  
-  //let a = parseInt(number1.value)
- // let b = parseInt(number2.value)
- // let x = parseInt(result.value)
-  //let summa = a + b
-  //let razn = a - b
-  //let pr = a * b
   let message = ""
   let backgroundColor = "lightgreen"
   let selectedValue = document.getElementById("action").value;
